6e1f076987351523bbb02d4b1173cd10772388df
ShinnyD <shinobuduerloo@gmail.com>
2023-08-04 12:25:36 +0200

    creates rows div classes and links them with click eventlistener, adds styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,49 @@
-const addition = ((a, b) => {
-    console.log(3 + 5);
+const numInput = document.querySelector(".numInput");
+const allButtons = document.querySelectorAll(".button");
+
+allButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const value = e.target.dataset.value;
+        numInput.textContent = value
+        console.log("e", e.target.dataset.value)
+    })
 })
+/*
+//let firstNumber = parseInt(prompt("Please input the first number:"));
+//let secondNumber = parseInt(prompt("Please input the second number:"));
 
-const subtraction = ((a, b) => {
-    return (3 - 5);
-}) 
+let operator = ["+", "-", "*", "/"];
 
-const multiplication = ((a, b) => {
-    return (3 * 5);
-})
+const addition = ((firstNumber, secondNumber) => {
+    let addValues = (firstNumber + secondNumber);
+    console.log("add", addValues);
+});
 
-const division = ((a, b) => {
-    return (15 / 3);
-})
+const subtraction = ((firstNumber, secondNumber) => {
+    let subtractValues = (firstNumber - secondNumber);
+    console.log("subtract", subtractValues);
+}); 
 
-let firstNumber = Number("5");
-let secondNumber = Number("3");
-let operator = {add:"+", subtract: "-", multiply: "*", divide: "/"};
+const multiplication = ((firstNumber, secondNumber) => {
+    let multiplyValues = (firstNumber * secondNumber);
+    console.log("multiply", multiplyValues);
+});
 
-const operate = ((firstNumber, secondNumber)=> {
+const division = ((firstNumber, secondNumber) => {
+    let divideValules = (firstNumber / secondNumber);
+    console.log("divide", divideValules);
+});
+
+addition(firstNumber, secondNumber);
+subtraction(firstNumber, secondNumber);
+multiplication(firstNumber, secondNumber);
+division(firstNumber, secondNumber);
+
+
+
+/*const operate = ((firstNumber, secondNumber)=> {
     if (operator === "+"){
         return firstNumber + secondNumber;
-        console.log(firstNumber + secondNumber);
+        console.log(operate);
     }
-})
+})*/
